7dfcdf19da5bc1bf5cca63a4c6740efdd6a42c89
amaralcs <amaralcscgp@gmail.com>
2021-08-16 22:41:02 +0100

    template for board and test_board

---
+++ src/env/board.py
@@ -0,0 +1,8 @@
+"""
+
+    Placeholder for a board class.
+
+    The board is where dice are rolled and their values are displayed.
+    Agents can interact with dice via the board.
+
+"""
---
+++ tests/env/test_board.py
@@ -0,0 +1,16 @@
+"""
+
+    The board should:
+        - Initialize 6 distinct dice 
+            - All dice should have no value at the start
+            - All dice should be unpicked at the start
+        - Display values of currently rolled dice
+        - Track turn of the active player
+        - Track in-turn score
+        - Track overall score
+        - Track which dice have been taken in each turn
+        - Track the number of turns
+        - Reset status of dice when a turn ends
+        - Reset status of dice when a player takes all 6 dice
+
+"""
